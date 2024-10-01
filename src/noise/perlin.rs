b2f245bcadcf90b643c9ce0946e314e7ec3059d8
Metaa4245 <cappymeta@gmail.com>
2024-10-01 17:27:53 +0200

    WIP populate_noise_array

--- src/noise/perlin.rs
+++ src/noise/perlin.rs
@@ -34,11 +34,16 @@ impl Perlin {
         }
     }
 
-    pub fn lerp(x: f64, y: f64, z: f64) -> f64 {
+    // what do i name this
+    fn idk(x: f64) -> f64 {
+        x * x * x * (x * (x * 6.0 - 15.0) + 10.0)
+    }
+
+    fn lerp(x: f64, y: f64, z: f64) -> f64 {
         y + x * (z - y)
     }
 
-    pub fn grad(a: i32, x: f64, y: f64, z: f64) -> f64 {
+    fn grad(a: i32, x: f64, y: f64, z: f64) -> f64 {
         let a = a & 15;
         let b = if a < 8 { x } else { y };
         let c = if a < 4 {
@@ -82,9 +87,9 @@ impl Perlin {
         y -= y_trunc as f64;
         z -= z_trunc as f64;
 
-        let temp1 = x * x * x * (x * (x * 6.0 - 15.0) + 10.0);
-        let temp2 = y * y * y * (y * (y * 6.0 - 15.0) + 10.0);
-        let temp3 = z * z * z * (z * (z * 6.0 - 15.0) + 10.0);
+        let temp1 = Self::idk(x);
+        let temp2 = Self::idk(y);
+        let temp3 = Self::idk(z);
 
         let perm1 = self.permutations[x_and as usize] + y_and as i32;
         let perm2 = self.permutations[perm1 as usize] + z_and as i32;
@@ -128,5 +133,80 @@ impl Perlin {
                 ),
             ),
         )
+    }
+
+    pub fn populate_noise_array(
+        &self,
+        var1: Vec<f64>,
+        var2: f64,
+        var4: f64,
+        var6: f64,
+        var8: f64,
+        y: i32,
+        var10: i32,
+        z: f64,
+        var13: f64,
+        var15: f64,
+        var17: f64,
+    ) {
+        let var19 = 0;
+        let var20 = 1.0 / var17;
+        let mut var22 = -1;
+        let var23 = false;
+        let var24 = false;
+        let var25 = false;
+        let var26 = false;
+        let var27 = false;
+        let var28 = false;
+        let var29 = 0.0;
+        let var31 = 0.0;
+        let var33 = 0.0;
+        let var35 = 0.0;
+
+        for var37 in 0..var8.trunc() as i64 {
+            let mut var38 = (var2 + var37 as f64) * z + self.x;
+            let mut var40 = var38.trunc() as i64;
+            if var38 < var40 as f64 {
+                var40 -= 1;
+            }
+
+            let var41 = var40 & 255;
+            var38 -= var40 as f64;
+            let var42 = Self::idk(var38);
+
+            for var44 in 0..var10 {
+                let mut var45 = (var6 + var44 as f64) * var15 + self.z;
+                let mut var47 = var45.trunc() as i64;
+                if var45 < var47 as f64 {
+                    var47 -= 1;
+                }
+
+                let var48 = var47 & 255;
+                var45 -= var47 as f64;
+                let var49 = Self::idk(var45);
+
+                for var51 in 0..y {
+                    let mut var52 = (var4 + var51 as f64) * var13 + self.y;
+                    let mut var54 = var52.trunc() as i64;
+                    if var52 < var54 as f64 {
+                        var54 -= 1;
+                    }
+
+                    let var55 = var54 & 255;
+                    var52 -= var54 as f64;
+                    let var56 = Self::idk(var52);
+
+                    if var51 == 0 || var55 != var22 {
+                        var22 = var55;
+                        let var64 = self.permutations[var41 as usize] + var55 as i32;
+                        let var65 = self.permutations[var64 as usize] + var48 as i32;
+                        let var66 = self.permutations[var64 as usize + 1] + var48 as i32;
+                        let var67 = self.permutations[var41 as usize + 1] + var55 as i32;
+                        let var68 = self.permutations[var67 as usize] + var48 as i32;
+                        let var69 = self.permutations[var67 as usize + 1] + var48 as i32;
+                    }
+                }
+            }
+        }
     }
 }
