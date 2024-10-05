87d3743148708519a7e32e7fee11a3f2ab4c3465
Metaa4245 <cappymeta@gmail.com>
2024-10-05 16:15:38 +0200

    add serialization to packets

--- src/network/packet.rs
+++ src/network/packet.rs
@@ -47,10 +47,15 @@ pub enum PacketId {
 }
 
 #[derive(Serialize, Deserialize)]
-pub struct KeepAlive {}
+pub struct KeepAlive {
+    #[serde(skip_deserializing)]
+    pub packet_id: i8,
+}
 
 #[derive(Serialize, Deserialize)]
 pub struct Login {
+    #[serde(skip_deserializing)]
+    pub packet_id: i8,
     pub protocol_version: i32,
     pub username: String,
     pub password: String,
@@ -58,16 +63,22 @@ pub struct Login {
 
 #[derive(Serialize, Deserialize)]
 pub struct Handshake {
+    #[serde(skip_deserializing)]
+    pub packet_id: i8,
     pub username: String,
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct Chat {
+    #[serde(skip_deserializing)]
+    pub packet_id: i8,
     pub message: String,
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct UpdateTime {
+    #[serde(skip_deserializing)]
+    pub packet_id: i8,
     pub time: i64,
 }
 
@@ -75,6 +86,8 @@ pub struct UpdateTime {
 
 #[derive(Serialize, Deserialize)]
 pub struct SpawnPosition {
+    #[serde(skip_deserializing)]
+    pub packet_id: i8,
     pub x: i32,
     pub y: i32,
     pub z: i32,
@@ -82,11 +95,15 @@ pub struct SpawnPosition {
 
 #[derive(Serialize, Deserialize)]
 pub struct Flying {
+    #[serde(skip_deserializing)]
+    pub packet_id: i8,
     pub on_ground: bool,
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct PlayerPosition {
+    #[serde(skip_deserializing)]
+    pub packet_id: i8,
     pub x: f64,
     pub y: f64,
     pub z: f64,
@@ -97,6 +114,8 @@ pub struct PlayerPosition {
 
 #[derive(Serialize, Deserialize)]
 pub struct PlayerLook {
+    #[serde(skip_deserializing)]
+    pub packet_id: i8,
     pub yaw: f32,
     pub pitch: f32,
     pub rotating: bool,
@@ -105,6 +124,8 @@ pub struct PlayerLook {
 
 #[derive(Serialize, Deserialize)]
 pub struct PlayerLookMove {
+    #[serde(skip_deserializing)]
+    pub packet_id: i8,
     pub x: f64,
     pub y: f64,
     pub z: f64,
@@ -118,6 +139,8 @@ pub struct PlayerLookMove {
 
 #[derive(Serialize, Deserialize)]
 pub struct BlockDig {
+    #[serde(skip_deserializing)]
+    pub packet_id: i8,
     pub x: i32,
     pub y: i8,
     pub z: i32,
@@ -127,6 +150,8 @@ pub struct BlockDig {
 
 #[derive(Serialize, Deserialize)]
 pub struct Place {
+    #[serde(skip_deserializing)]
+    pub packet_id: i8,
     pub id: i16,
     pub x: i32,
     pub y: i8,
@@ -136,12 +161,16 @@ pub struct Place {
 
 #[derive(Serialize, Deserialize)]
 pub struct BlockItemSwitch {
+    #[serde(skip_deserializing)]
+    pub packet_id: i8,
     pub entity_id: i32,
     pub id: i16,
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct AddToInventory {
+    #[serde(skip_deserializing)]
+    pub packet_id: i8,
     pub item_id: i16,
     pub count: i8,
     pub item_damage: i16,
@@ -149,12 +178,16 @@ pub struct AddToInventory {
 
 #[derive(Serialize, Deserialize)]
 pub struct ArmAnimation {
+    #[serde(skip_deserializing)]
+    pub packet_id: i8,
     pub entity_id: i32,
     pub animate: i8,
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct NamedEntitySpawn {
+    #[serde(skip_deserializing)]
+    pub packet_id: i8,
     pub entity_id: i32,
     pub current_item: i16,
     pub name: String,
@@ -167,6 +200,8 @@ pub struct NamedEntitySpawn {
 
 #[derive(Serialize, Deserialize)]
 pub struct PickupSpawn {
+    #[serde(skip_deserializing)]
+    pub packet_id: i8,
     pub entity_id: i32,
     pub item_id: i16,
     pub count: i8,
@@ -179,13 +214,18 @@ pub struct PickupSpawn {
 }
 
 #[derive(Serialize, Deserialize)]
+#[allow(clippy::struct_field_names)]
 pub struct Collect {
+    #[serde(skip_deserializing)]
+    pub packet_id: i8,
     pub collected_entity_id: i32,
     pub collector_entity_id: i32,
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct VehicleSpawn {
+    #[serde(skip_deserializing)]
+    pub packet_id: i8,
     pub entity_id: i32,
     pub entity_type: i8,
     pub x: i32,
@@ -195,6 +235,8 @@ pub struct VehicleSpawn {
 
 #[derive(Serialize, Deserialize)]
 pub struct MobSpawn {
+    #[serde(skip_deserializing)]
+    pub packet_id: i8,
     pub entity_id: i32,
     pub entity_type: i8,
     pub x: i32,
@@ -206,16 +248,22 @@ pub struct MobSpawn {
 
 #[derive(Serialize, Deserialize)]
 pub struct DestroyEntity {
+    #[serde(skip_deserializing)]
+    pub packet_id: i8,
     pub entity_id: i32,
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct Entity {
+    #[serde(skip_deserializing)]
+    pub packet_id: i8,
     pub entity_id: i32,
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct RelEntityMove {
+    #[serde(skip_deserializing)]
+    pub packet_id: i8,
     pub entity_id: i32,
     pub x: i8,
     pub y: i8,
@@ -224,6 +272,8 @@ pub struct RelEntityMove {
 
 #[derive(Serialize, Deserialize)]
 pub struct EntityLook {
+    #[serde(skip_deserializing)]
+    pub packet_id: i8,
     pub entity_id: i32,
     pub yaw: i8,
     pub pitch: i8,
@@ -231,6 +281,8 @@ pub struct EntityLook {
 
 #[derive(Serialize, Deserialize)]
 pub struct RelEntityMoveLook {
+    #[serde(skip_deserializing)]
+    pub packet_id: i8,
     pub entity_id: i32,
     pub x: i8,
     pub y: i8,
@@ -241,6 +293,8 @@ pub struct RelEntityMoveLook {
 
 #[derive(Serialize, Deserialize)]
 pub struct EntityTeleport {
+    #[serde(skip_deserializing)]
+    pub packet_id: i8,
     pub entity_id: i32,
     pub x: i32,
     pub y: i32,
@@ -251,6 +305,8 @@ pub struct EntityTeleport {
 
 #[derive(Serialize, Deserialize)]
 pub struct PreChunk {
+    #[serde(skip_deserializing)]
+    pub packet_id: i8,
     pub x: i32,
     pub z: i32,
     pub mode: bool,
@@ -258,18 +314,23 @@ pub struct PreChunk {
 
 #[derive(Serialize, Deserialize)]
 pub struct KickDisconnect {
+    #[serde(skip_deserializing)]
+    pub packet_id: i8,
     pub reason: String,
 }
 
 impl Packet<Self> for KeepAlive {
     async fn read(_stream: &mut TcpStream) -> Result<Self> {
-        Ok(Self {})
+        Ok(Self {
+            packet_id: PacketId::KeepAlive as i8,
+        })
     }
 }
 
 impl Packet<Self> for Login {
     async fn read(stream: &mut TcpStream) -> Result<Self> {
         Ok(Self {
+            packet_id: PacketId::Login as i8,
             protocol_version: stream.read_i32().await?,
             username: stream.read_string().await?,
             password: stream.read_string().await?,
@@ -280,6 +341,7 @@ impl Packet<Self> for Login {
 impl Packet<Self> for Handshake {
     async fn read(stream: &mut TcpStream) -> Result<Self> {
         Ok(Self {
+            packet_id: PacketId::Handshake as i8,
             username: stream.read_string().await?,
         })
     }
@@ -288,6 +350,7 @@ impl Packet<Self> for Handshake {
 impl Packet<Self> for Chat {
     async fn read(stream: &mut TcpStream) -> Result<Self> {
         Ok(Self {
+            packet_id: PacketId::Chat as i8,
             message: stream.read_string().await?,
         })
     }
@@ -296,6 +359,7 @@ impl Packet<Self> for Chat {
 impl Packet<Self> for UpdateTime {
     async fn read(stream: &mut TcpStream) -> Result<Self> {
         Ok(Self {
+            packet_id: PacketId::UpdateTime as i8,
             time: stream.read_long().await?,
         })
     }
@@ -306,6 +370,7 @@ impl Packet<Self> for UpdateTime {
 impl Packet<Self> for SpawnPosition {
     async fn read(stream: &mut TcpStream) -> Result<Self> {
         Ok(Self {
+            packet_id: PacketId::SpawnPosition as i8,
             x: stream.read_int().await?,
             y: stream.read_int().await?,
             z: stream.read_int().await?,
@@ -316,6 +381,7 @@ impl Packet<Self> for SpawnPosition {
 impl Packet<Self> for Flying {
     async fn read(stream: &mut TcpStream) -> Result<Self> {
         Ok(Self {
+            packet_id: PacketId::Flying as i8,
             on_ground: stream.read_bool().await?,
         })
     }
@@ -324,6 +390,7 @@ impl Packet<Self> for Flying {
 impl Packet<Self> for PlayerPosition {
     async fn read(stream: &mut TcpStream) -> Result<Self> {
         Ok(Self {
+            packet_id: PacketId::PlayerPosition as i8,
             x: stream.read_double().await?,
             y: stream.read_double().await?,
             stance: stream.read_double().await?,
@@ -337,6 +404,7 @@ impl Packet<Self> for PlayerPosition {
 impl Packet<Self> for PlayerLook {
     async fn read(stream: &mut TcpStream) -> Result<Self> {
         Ok(Self {
+            packet_id: PacketId::PlayerLook as i8,
             yaw: stream.read_float().await?,
             pitch: stream.read_float().await?,
             rotating: true,
@@ -348,6 +416,7 @@ impl Packet<Self> for PlayerLook {
 impl Packet<Self> for PlayerLookMove {
     async fn read(stream: &mut TcpStream) -> Result<Self> {
         Ok(Self {
+            packet_id: PacketId::PlayerLookMove as i8,
             x: stream.read_double().await?,
             y: stream.read_double().await?,
             stance: stream.read_double().await?,
@@ -364,6 +433,7 @@ impl Packet<Self> for PlayerLookMove {
 impl Packet<Self> for BlockDig {
     async fn read(stream: &mut TcpStream) -> Result<Self> {
         Ok(Self {
+            packet_id: PacketId::BlockDig as i8,
             status: stream.read_byte().await?,
             x: stream.read_int().await?,
             y: stream.read_byte().await?,
@@ -376,6 +446,7 @@ impl Packet<Self> for BlockDig {
 impl Packet<Self> for Place {
     async fn read(stream: &mut TcpStream) -> Result<Self> {
         Ok(Self {
+            packet_id: PacketId::Place as i8,
             id: stream.read_short().await?,
             x: stream.read_int().await?,
             y: stream.read_byte().await?,
@@ -388,6 +459,7 @@ impl Packet<Self> for Place {
 impl Packet<Self> for BlockItemSwitch {
     async fn read(stream: &mut TcpStream) -> Result<Self> {
         Ok(Self {
+            packet_id: PacketId::BlockItemSwitch as i8,
             entity_id: stream.read_int().await?,
             id: stream.read_short().await?,
         })
@@ -397,6 +469,7 @@ impl Packet<Self> for BlockItemSwitch {
 impl Packet<Self> for AddToInventory {
     async fn read(stream: &mut TcpStream) -> Result<Self> {
         Ok(Self {
+            packet_id: PacketId::AddToInventory as i8,
             item_id: stream.read_short().await?,
             count: stream.read_byte().await?,
             item_damage: stream.read_short().await?,
@@ -407,6 +480,7 @@ impl Packet<Self> for AddToInventory {
 impl Packet<Self> for ArmAnimation {
     async fn read(stream: &mut TcpStream) -> Result<Self> {
         Ok(Self {
+            packet_id: PacketId::ArmAnimation as i8,
             entity_id: stream.read_int().await?,
             animate: stream.read_byte().await?,
         })
@@ -416,6 +490,7 @@ impl Packet<Self> for ArmAnimation {
 impl Packet<Self> for NamedEntitySpawn {
     async fn read(stream: &mut TcpStream) -> Result<Self> {
         Ok(Self {
+            packet_id: PacketId::NamedEntitySpawn as i8,
             entity_id: stream.read_int().await?,
             name: stream.read_string().await?,
             x: stream.read_int().await?,
@@ -431,6 +506,7 @@ impl Packet<Self> for NamedEntitySpawn {
 impl Packet<Self> for PickupSpawn {
     async fn read(stream: &mut TcpStream) -> Result<Self> {
         Ok(Self {
+            packet_id: PacketId::PickupSpawn as i8,
             entity_id: stream.read_int().await?,
             item_id: stream.read_short().await?,
             count: stream.read_byte().await?,
@@ -447,6 +523,7 @@ impl Packet<Self> for PickupSpawn {
 impl Packet<Self> for Collect {
     async fn read(stream: &mut TcpStream) -> Result<Self> {
         Ok(Self {
+            packet_id: PacketId::Collect as i8,
             collected_entity_id: stream.read_int().await?,
             collector_entity_id: stream.read_int().await?,
         })
@@ -456,6 +533,7 @@ impl Packet<Self> for Collect {
 impl Packet<Self> for VehicleSpawn {
     async fn read(stream: &mut TcpStream) -> Result<Self> {
         Ok(Self {
+            packet_id: PacketId::VehicleSpawn as i8,
             entity_id: stream.read_int().await?,
             entity_type: stream.read_byte().await?,
             x: stream.read_int().await?,
@@ -468,6 +546,7 @@ impl Packet<Self> for VehicleSpawn {
 impl Packet<Self> for MobSpawn {
     async fn read(stream: &mut TcpStream) -> Result<Self> {
         Ok(Self {
+            packet_id: PacketId::MobSpawn as i8,
             entity_id: stream.read_int().await?,
             entity_type: stream.read_byte().await?,
             x: stream.read_int().await?,
@@ -482,6 +561,7 @@ impl Packet<Self> for MobSpawn {
 impl Packet<Self> for DestroyEntity {
     async fn read(stream: &mut TcpStream) -> Result<Self> {
         Ok(Self {
+            packet_id: PacketId::DestroyEntity as i8,
             entity_id: stream.read_int().await?,
         })
     }
@@ -490,6 +570,7 @@ impl Packet<Self> for DestroyEntity {
 impl Packet<Self> for Entity {
     async fn read(stream: &mut TcpStream) -> Result<Self> {
         Ok(Self {
+            packet_id: PacketId::Entity as i8,
             entity_id: stream.read_int().await?,
         })
     }
@@ -498,6 +579,7 @@ impl Packet<Self> for Entity {
 impl Packet<Self> for RelEntityMove {
     async fn read(stream: &mut TcpStream) -> Result<Self> {
         Ok(Self {
+            packet_id: PacketId::RelEntityMove as i8,
             entity_id: stream.read_int().await?,
             x: stream.read_byte().await?,
             y: stream.read_byte().await?,
@@ -509,6 +591,7 @@ impl Packet<Self> for RelEntityMove {
 impl Packet<Self> for EntityLook {
     async fn read(stream: &mut TcpStream) -> Result<Self> {
         Ok(Self {
+            packet_id: PacketId::EntityLook as i8,
             entity_id: stream.read_int().await?,
             yaw: stream.read_byte().await?,
             pitch: stream.read_byte().await?,
@@ -519,6 +602,7 @@ impl Packet<Self> for EntityLook {
 impl Packet<Self> for RelEntityMoveLook {
     async fn read(stream: &mut TcpStream) -> Result<Self> {
         Ok(Self {
+            packet_id: PacketId::RelEntityMoveLook as i8,
             entity_id: stream.read_int().await?,
             x: stream.read_byte().await?,
             y: stream.read_byte().await?,
@@ -532,6 +616,7 @@ impl Packet<Self> for RelEntityMoveLook {
 impl Packet<Self> for EntityTeleport {
     async fn read(stream: &mut TcpStream) -> Result<Self> {
         Ok(Self {
+            packet_id: PacketId::EntityTeleport as i8,
             entity_id: stream.read_int().await?,
             x: stream.read_int().await?,
             y: stream.read_int().await?,
@@ -545,6 +630,7 @@ impl Packet<Self> for EntityTeleport {
 impl Packet<Self> for PreChunk {
     async fn read(stream: &mut TcpStream) -> Result<Self> {
         Ok(Self {
+            packet_id: PacketId::PreChunk as i8,
             x: stream.read_int().await?,
             z: stream.read_int().await?,
             mode: stream.read_bool().await?,
@@ -555,6 +641,7 @@ impl Packet<Self> for PreChunk {
 impl Packet<Self> for KickDisconnect {
     async fn read(stream: &mut TcpStream) -> Result<Self> {
         Ok(Self {
+            packet_id: PacketId::KickDisconnect as i8,
             reason: stream.read_string().await?,
         })
     }
